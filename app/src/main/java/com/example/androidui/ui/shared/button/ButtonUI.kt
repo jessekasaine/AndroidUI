b101dde2875a4926e067c0a23a0d5bba3c47ba1c
Jesse Saita Kasaine <saitakasaine@gmail.com>
2026-08-19 12:43:51 +0300

    add buttons

---
+++ app/src/main/java/com/example/androidui/ui/shared/button/ButtonUI.kt
@@ -0,0 +1,2 @@
+package com.example.androidui.ui.shared.button
+
---
+++ app/src/main/java/com/example/androidui/ui/shared/button/ButtonUIViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.androidui.ui.shared.button
+
+import androidx.lifecycle.ViewModel
+
+class ButtonUIViewModel: ViewModel() {
+}
